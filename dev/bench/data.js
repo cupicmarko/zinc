081c46e98b630fe4805a309a00104113c14c31f9
github-action-benchmark <github@users.noreply.github.com>
2025-08-12 19:49:29 +0000

    add Benchmark (cargo) benchmark result for 2ee8a9773ee12d9dddcdfae36d74f5a562f4eaa5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755011794125,
+  "lastUpdate": 1755028169317,
   "repoUrl": "https://github.com/cupicmarko/zinc",
   "entries": {
     "Benchmark": [
@@ -347,6 +347,354 @@ window.BENCHMARK_DATA = {
             "name": "Zip/Verify: RandomField<4>, poly_size = 2^16(Int limbs = 1), ZipSpec1, modulus=106319353542452952636...",
             "value": 461150543,
             "range": "± 283307",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "71461781+cupicmarko@users.noreply.github.com",
+            "name": "Marko Cupic",
+            "username": "cupicmarko"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2ee8a9773ee12d9dddcdfae36d74f5a562f4eaa5",
+          "message": "Merge pull request #2 from cupicmarko/ci/benchmarks\n\nCi/benchmarks",
+          "timestamp": "2025-08-12T16:34:23+02:00",
+          "tree_id": "333f4f60f8315e56b7c763d400e9f0b8f5eea0c7",
+          "url": "https://github.com/cupicmarko/zinc/commit/2ee8a9773ee12d9dddcdfae36d74f5a562f4eaa5"
+        },
+        "date": 1755028168953,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "PrimeGen/PrimeGen/196bits",
+            "value": 2349825,
+            "range": "± 7443",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "RandomFieldArithmetic/Multiply/Random128BitFieldElement",
+            "value": 275411,
+            "range": "± 1162",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "RandomFieldArithmetic/Addition/Random128BitFieldElement",
+            "value": 81120,
+            "range": "± 729",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "RandomFieldArithmetic/Division/Random128BitFieldElement",
+            "value": 13820916,
+            "range": "± 39565",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "RandomFieldArithmetic/Negation/Random128BitFieldElement",
+            "value": 21749,
+            "range": "± 205",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "RandomFieldArithmetic/Sum/Random128BitFieldElement",
+            "value": 1952971,
+            "range": "± 2567",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "RandomFieldArithmetic/Product/Random128BitFieldElement",
+            "value": 3876543,
+            "range": "± 25502",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_prover for 256 prime/n=4096",
+            "value": 11935560,
+            "range": "± 28655",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_prover for 256 prime/n=8192",
+            "value": 23803073,
+            "range": "± 59292",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_prover for 256 prime/n=16384",
+            "value": 48167946,
+            "range": "± 181739",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_prover for 256 prime/n=32768",
+            "value": 97457975,
+            "range": "± 400636",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_prover for 256 prime/n=65536",
+            "value": 199717721,
+            "range": "± 370375",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_verifier for 256 prime/n=4096",
+            "value": 496269,
+            "range": "± 1328",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_verifier for 256 prime/n=8192",
+            "value": 551456,
+            "range": "± 5752",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_verifier for 256 prime/n=16384",
+            "value": 592221,
+            "range": "± 1161",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_verifier for 256 prime/n=32768",
+            "value": 642549,
+            "range": "± 2210",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_verifier for 256 prime/n=65536",
+            "value": 680538,
+            "range": "± 1203",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_prover for stark prime/n=4096",
+            "value": 12109711,
+            "range": "± 39275",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_prover for stark prime/n=8192",
+            "value": 24257914,
+            "range": "± 54134",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_prover for stark prime/n=16384",
+            "value": 47920655,
+            "range": "± 375867",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_prover for stark prime/n=32768",
+            "value": 95638843,
+            "range": "± 276262",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_prover for stark prime/n=65536",
+            "value": 193624180,
+            "range": "± 851809",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_verifier for stark prime/n=4096",
+            "value": 698841,
+            "range": "± 1927",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_verifier for stark prime/n=8192",
+            "value": 761808,
+            "range": "± 1956",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_verifier for stark prime/n=16384",
+            "value": 824955,
+            "range": "± 1118",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_verifier for stark prime/n=32768",
+            "value": 886045,
+            "range": "± 2394",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spartan_verifier for stark prime/n=65536",
+            "value": 945864,
+            "range": "± 1792",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Sumcheck Benchmarks/Sumcheck/Prime 1",
+            "value": 16743203188,
+            "range": "± 43252772",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Sumcheck Benchmarks/Sumcheck/Prime 2",
+            "value": 14894318558,
+            "range": "± 26629219",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Sumcheck Benchmarks/Sumcheck/Prime 3",
+            "value": 15630007824,
+            "range": "± 37759354",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Sumcheck Benchmarks/Sumcheck/Prime 4",
+            "value": 16969611202,
+            "range": "± 30645579",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Sumcheck Benchmarks/Sumcheck/Prime 5",
+            "value": 13553350268,
+            "range": "± 64617264",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Sumcheck Benchmarks/Sumcheck/Prime 6",
+            "value": 15798871259,
+            "range": "± 24544999",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Sumcheck Benchmarks/Sumcheck/Prime 7",
+            "value": 16155780325,
+            "range": "± 17191853",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Sumcheck Benchmarks/Sumcheck/Prime 8",
+            "value": 17135452178,
+            "range": "± 35683574",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Sumcheck Benchmarks/Sumcheck/Prime 9",
+            "value": 15915883048,
+            "range": "± 108926960",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Sumcheck Benchmarks/Sumcheck/Prime 10",
+            "value": 16895481397,
+            "range": "± 41686075",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Core/Zip/EncodeRows: Int<4>, poly_size = 2^12(Int limbs = 1), ZipSpec1",
+            "value": 412257,
+            "range": "± 1022",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Core/Zip/EncodeMessage: Int<4>, row_len = 128(Int limbs = 1), ZipSpec1",
+            "value": 13934,
+            "range": "± 13",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Core/Zip/MerkleRoot: Int<1>, leaves=2^12, spec=1",
+            "value": 882282,
+            "range": "± 2512",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Core/Zip/Commit: Int<4>, poly_size = 2^12(Int limbs = 1), ZipSpec1",
+            "value": 95199,
+            "range": "± 240",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Core/Zip/Open: RandomField<4>, poly_size = 2^12(Int limbs = 1), ZipSpec1, modulus=106319353542452952...",
+            "value": 3522132,
+            "range": "± 34309",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Core/Zip/Verify: RandomField<4>, poly_size = 2^12(Int limbs = 1), ZipSpec1, modulus=1063193535424529...",
+            "value": 99903475,
+            "range": "± 148141",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Zip/EncodeRows: Int<4>, poly_size = 2^16(Int limbs = 1), ZipSpec1",
+            "value": 6553064,
+            "range": "± 22511",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Zip/EncodeMessage: Int<4>, row_len = 256(Int limbs = 1), ZipSpec1",
+            "value": 27109,
+            "range": "± 139",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Zip/EncodeMessage: Int<4>, row_len = 512(Int limbs = 1), ZipSpec1",
+            "value": 54386,
+            "range": "± 57",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Zip/EncodeMessage: Int<4>, row_len = 1024(Int limbs = 1), ZipSpec1",
+            "value": 110189,
+            "range": "± 330",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Zip/EncodeMessage: Int<4>, row_len = 2048(Int limbs = 1), ZipSpec1",
+            "value": 220135,
+            "range": "± 688",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Zip/EncodeMessage: Int<4>, row_len = 4096(Int limbs = 1), ZipSpec1",
+            "value": 442081,
+            "range": "± 550",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Zip/MerkleRoot: Int<1>, leaves=2^16, spec=1",
+            "value": 14152612,
+            "range": "± 70718",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Zip/MerkleRoot: Int<1>, leaves=2^22, spec=1",
+            "value": 928509163,
+            "range": "± 1403579",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Zip/Commit: Int<4>, poly_size = 2^16(Int limbs = 1), ZipSpec1",
+            "value": 17546320,
+            "range": "± 48925",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Zip/Open: RandomField<4>, poly_size = 2^16(Int limbs = 1), ZipSpec1, modulus=10631935354245295263634...",
+            "value": 123585636,
+            "range": "± 2208324",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Zip/Verify: RandomField<4>, poly_size = 2^16(Int limbs = 1), ZipSpec1, modulus=106319353542452952636...",
+            "value": 446877809,
+            "range": "± 1920410",
             "unit": "ns/iter"
           }
         ]
